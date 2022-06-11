e05f9c6023b28992d16b9475ab1ca9c8f24e79b7
Aba Wandjovu Nicaisse <abawandjovunicaise@gmail.com>
2022-06-11 10:16:23 +0300

    Line Chart Implemented

--- src/pages/Charts/Line.jsx
+++ src/pages/Charts/Line.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
+import { Header, LineChart } from "../../components";
+
 const Line = () => {
-  return <div>Line</div>;
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl dark:bg-secondary-dark-bg">
+      <Header category={"Charts"} title="Inflation Rate" />
+
+      <div className="w-full">
+        <LineChart />
+      </div>
+    </div>
+  );
 };
 
 export default Line;
